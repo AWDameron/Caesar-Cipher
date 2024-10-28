b360657b4f6141604e8761315b2ef28e60ea7613
Alex Dameron <AWDameron@gmail.com>
2024-10-27 20:00:13 -0400

    updated cipher.py, added functionality for encryption

--- cipher.py
+++ cipher.py
@@ -1,1 +1,33 @@
-# add your code here
+
+# Dress your text up to the Caesar's preferred method of encryption, the default value for this 
+# program is 5, but if you'd like to use a different shift you can simply call the function as
+# Caesar_Dress(sentence,integer_here).   
+
+def Caesar_Dressing (sentence, shift=5):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    sentence_index = 0 
+    # used print for checking program as it's being written: print(sentence)
+    sentence_list = [letter.lower() for letter in sentence]
+    # used print for checking program as it's being written: print(sentence_list)
+    for letter in sentence_list:
+        if letter in letters:
+            index = letters.index(letter)
+            if index + shift >= 26:
+                new_index = (index+shift) % 26
+                sentence_list[sentence_index] = letters[index]
+                sentence_index += 1
+            else:
+                sentence_list[sentence_index] = letters[index+shift]
+                sentence_index += 1
+        else:
+            sentence_index += 1
+    # used print for checking program as it's being written: print(sentence_list)
+    coded_sentence = "".join(sentence_list) 
+    
+    return coded_sentence
+
+
+sentence = 'I love coding in python'
+print(sentence)
+print(Caesar_Dressing(sentence))
+
